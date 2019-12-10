dda4ec40886a8b5336f1777496d732db363c776a
Jessica Giang <giang@aclanyc.com>
2019-12-10 13:29:05 +0000

    added comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
     $("#button").click(function(){
+        //clears the resultant so that it doesn't append
         $("#output").text(" ");
         let string = $(".initial").val();
         lowerCaseString = string.toLowerCase();
         let words = lowerCaseString.split(" ");
+        //makes the 3 parts of the result
         $("#user").append(`"${lowerCaseString}"`);
         $("#becomes").append(" becomes ");
         words.forEach(function(word){
