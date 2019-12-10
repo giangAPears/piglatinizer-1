903024f3679dba9204119b57fa23c76e45bfb56e
Jessica Giang <giang@aclanyc.com>
2019-12-10 13:19:29 +0000

    completed first letter to the end

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-
-
-
-  
-  
-  
-  
+    $("#button").click(function(){
+        $("#output").text(" ");
+        let string = $(".initial").val();
+        lowerCaseString = string.toLowerCase();
+        let words = lowerCaseString.split(" ");
+        $("#user").append(`"${lowerCaseString}"`);
+        $("#becomes").append(" becomes ");
+        words.forEach(function(word){
+            let firstLetter = word.charAt(0);
+            // move the first letter to the end
+            let restOfWord = word.slice(1);
+            if(lowerCaseString==="" || lowerCaseString===" ") {
+                alert("Please type something in");
+                $(".initial").val("");
+            } else {
+            $("#output").append(`${restOfWord + firstLetter}ay `);
+            $(".initial").val("");
+            }
+        });
+    });
 });
